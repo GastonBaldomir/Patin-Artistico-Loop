ccbbaeb1c911d297f2e96ecf1e52635be2561bd8
PC <gasston327@gmail.com>
2026-01-31 14:28:19 -0300

    primer commit

---
+++ Loop-Patin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loop_Patin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
